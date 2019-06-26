8d61c915c874663274ffd8cca5bdd579d1927642
Avmary <avmary85@gmail.com>
2019-06-26 10:25:19 +0300

    fixed code

--- home_work/01/main.js
+++ home_work/01/main.js
@@ -1,5 +1,5 @@
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
-const newLogin = prompt('Введите логин!');
+// const newLogin = prompt('Введите логин!');
 function isLoginValid(login) {
   return login.length >= 4 && login.length <= 16;
 }
@@ -9,14 +9,11 @@ function isLoginUnique(allLogins, login) {
 }
 
 function addLogin(allLogins, login) {
-  isLoginValid(login);
-  console.log(isLoginValid(newLogin));
-  isLoginUnique(logins, login);
-  if (isLoginValid) {
+  if (!isLoginValid(login)) {
     alert('Ошибка! Логин должен быть от 4 до 16 символов');
-  } else if (!isLoginUnique) {
+  } else if (isLoginUnique(allLogins, login)) {
     alert('Такой логин уже используется!');
-  } else if (isLoginUnique) {
+  } else if (!isLoginUnique(allLogins, login)) {
     logins.push(login);
     alert('Логин успешно добавлен!');
   }
@@ -27,3 +24,4 @@ addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
 addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
 addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(logins);

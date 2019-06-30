f0265c3f6ac4dcc8d82181c22fe078845493695d
Avmary <avmary85@gmail.com>
2019-06-30 22:07:07 +0300

    finished 3 practice task

---
+++ practice/03/main.js
@@ -0,0 +1,14 @@
+const countProps = (obj) => {
+  const key = Object.keys(obj);
+  if (key.length > 0) {
+    return key.length;
+  } return 0;
+};
+
+console.log(
+  countProps({}),
+); // 0
+
+console.log(
+  countProps({ a: 1, b: 3, c: 'hello' }),
+); // 3

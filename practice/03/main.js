3918eb7a9297c426fffc132dbd7f8b18b0004be8
Avmary <avmary85@gmail.com>
2019-07-25 18:29:03 +0300

    add finall changes

--- practice/03/main.js
+++ practice/03/main.js
@@ -13,3 +13,18 @@ mango.getInfo(); // User Mango is 2 years old and has 20 friends
 
 const poly = new User('Poly', 3, 17);
 poly.getInfo(); // User Poly is 3 years old and has 17 friends
+
+
+let user = {
+  data: [
+    { name: 'T. Woods', age: 37 },
+    { name: 'P. Mickelson', age: 43 },
+  ],
+  clickHandler (event) {
+      var randomNum = ((Math.random () * 2 | 0) + 1) - 1; 
+//  Случайное число от 0 до 1.        
+//  Эта строка добавляет случайного человека из массива данных в текстовое поле.        
+$("input").val(this.data[randomNum].name + " " + this.data[randomNum].age);
+  },
+};
+//  Назначаем eventHandler на событие по клику на кнопку$ ("button").click (user.clickHandler);

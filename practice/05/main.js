e555e486e09fe48e53c25c89c23b55e9ad611af8
Avmary <avmary85@gmail.com>
2019-08-19 14:36:56 +0300

    fixed 5 practice task

--- practice/05/main.js
+++ practice/05/main.js
@@ -4,9 +4,18 @@ function collectInputData(inputs) {
   const checked = [...values].filter(e => e.checked);
   console.log(checked.map(e => e.value));
 }
-
 collectInputData(values);
 
+// =======еще вариант============================================================
+
+// const collectInputData = (inputs) => {
+//   const arrayInputs = [...inputs];
+//   return arrayInputs.map(e => e.value);
+// };
+// const inputs = document.querySelectorAll('input[type=checkbox]:checked');
+// console.log(collectInputData(inputs));
+
+// =======еще вариант============================================================
 
 // const inputs = document.querySelectorAll('.size-filter input[type="checkbox"]');
 

5e9f14eb2e9f8d1f1d3faf48d84a7a297882a849
Avmary <avmary85@gmail.com>
2019-09-05 00:31:12 +0300

    finished 3 practice task

--- practice/03/main.js
+++ practice/03/main.js
@@ -1,44 +1,49 @@
-const elements = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
-const list = document.querySelector('.list');
-function setListContent(arr) {
-  const item = arr.map((e) => {
-    const li = document.createElement('li');
-    li.textContent = e;
-    return li;
-  });
-  list.append(...item);
+const refs = {
+  add: document.querySelector('button[data-action="add"]'),
+  sub: document.querySelector('button[data-action="sub"]'),
+  result: document.querySelector('.value'),
+};
+
+function onChange(num) {
+  refs.result.textContent = num;
 }
-setListContent(elements);
 
-//= ============= 2 Вариант =====================================
-// const elements = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
-// const ref = {
-//   list: document.querySelector('.list'),
-// };
-// function setListContent(arr, parent) {
-//   const list = document.createElement('ul');
-//   list.classList.add('list');
-//   arr.map((e) => {
-//     const li = document.createElement('li');
-//     li.textContent = e;
-//     list.appendChild(li);
-//   });
-//   parent.replaceWith(list);
-// }
-// setListContent(elements, ref.list);
-//= ============= 3 Вариант =====================================
+class Counter {
+  constructor(onChange) {
+    this.value = 0;
+    this._onChange = onChange.bind(this);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
 
-// const elements = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
-// const ref = {
-//   list: document.querySelector('.list'),
-// };
-// function setListContent(arr, parent) {
-//   parent.innerHTML = '';
-//   const list = arr.map((e) => {
-//     const li = document.createElement('li');
-//     li.textContent = e;
-//     return li;
-//   });
-//   parent.append(...list);
+  increment() {
+    this.value += 1;
+    this._onChange(this.value);
+  }
+
+  decrement() {
+    this.value -= 1;
+    this._onChange(this.value);
+  }
+}
+// 2 вариант =============================================
+// class Counter {
+//   constructor(onChange) {
+//     this.value = 0;
+//     this._onChange = onChange.bind(this);
+//   }
+
+//   increment = () => {
+//     this.value += 1;
+//     this._onChange(this.value);
+//   }
+
+//   decrement = () => {
+//     this.value -= 1;
+//     this._onChange(this.value);
+//   }
 // }
-// setListContent(elements, ref.list);
+const counter = new Counter(onChange);
+
+refs.add.addEventListener('click', counter.increment);
+refs.sub.addEventListener('click', counter.decrement);

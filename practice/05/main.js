1c96249f2875e7c613159014529a916482b8eb74
Avmary <avmary85@gmail.com>
2019-09-05 01:27:43 +0300

    finished 5 practice task

--- practice/05/main.js
+++ practice/05/main.js
@@ -1,25 +1,7 @@
-const values = document.querySelectorAll('.size-filter input[type="checkbox"]');
-
-function collectInputData(inputs) {
-  const checked = [...values].filter(e => e.checked);
-  console.log(checked.map(e => e.value));
+const images = document.querySelector('.images');
+images.addEventListener('click', getImgSrc);
+function getImgSrc({ target }) {
+  event.preventDefault();
+  const { src } = target;
+  alert(src);
 }
-collectInputData(values);
-
-// =======еще вариант============================================================
-
-// const collectInputData = (inputs) => {
-//   const arrayInputs = [...inputs];
-//   return arrayInputs.map(e => e.value);
-// };
-// const inputs = document.querySelectorAll('input[type=checkbox]:checked');
-// console.log(collectInputData(inputs));
-
-// =======еще вариант============================================================
-
-// const inputs = document.querySelectorAll('.size-filter input[type="checkbox"]');
-
-// function collectInputData(inputs) {
-//   console.log([...inputs].filter(e => e.checked).map(e => e.value));
-// }
-// collectInputData(inputs);

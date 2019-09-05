208c888320f47bd2a1ad0ea63fe7a18d6107e132
Avmary <avmary85@gmail.com>
2019-09-05 19:36:39 +0300

    fixed some parts

--- home_work/01/src/js/controller/removeListItem.js
+++ home_work/01/src/js/controller/removeListItem.js
@@ -4,8 +4,8 @@ function removeListItem({ target }) {
   if (target.nodeName !== 'I') return;
   if (target.textContent === 'delete') {
     const li = target.closest('.note-list__item');
-    const id = li.dataset.id;
-    // const {id} = li.dataset;
+    // const id = li.dataset.id;
+    const { id } = li.dataset;
     li.remove();
     notepad.deleteNote(id);
     // console.log(notepad.notes);
